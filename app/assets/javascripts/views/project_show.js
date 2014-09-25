911b8dfaeb48d78d0665fdb3f966635a946965a0
Caslyn Cole <caslyn.cole@gmail.com>
2014-09-25 11:31:45 -0700

    refining project show toggling trackers

--- app/assets/javascripts/views/project_show.js
+++ app/assets/javascripts/views/project_show.js
@@ -1,17 +1,45 @@
 TaskTracker.Views.ProjectShow = Backbone.CompositeView.extend({
 	template: JST["projects/show"],
 
+	events: {
+		"click .tracker-btn": "toggleView"
+	},
+
 	initialize: function() {
 		this.collection = this.model.stories();
 		this.listenTo(this.model, 'add sync', this.render);
 		this.listenTo(this.collection, 'add', this.renderStories);
 	},
 
+	setViews: function() {
+		for (var key in window.localStorage) {
+			var $trackerBox = $('.tracker-container').find('.' + key);
+			$trackerBox.addClass('hidden');
+		}
+	},
+
+	toggleView: function(event) {
+
+		event.preventDefault;
+		var $trackerId = $(event.currentTarget).attr('id');
+		var $trackerBox = $('.tracker-container').find('.' + $trackerId)
+
+		if (window.localStorage.getItem($trackerId)) {
+			window.localStorage.removeItem($trackerId);
+			$trackerBox.removeClass('hidden');
+		} else {
+			window.localStorage.setItem($trackerId, 'hidden');
+			$trackerBox.addClass('hidden');
+		}
+	},
+
+	// Consider subviews
 	render: function() {
 		var renderedContent = this.template({
 			project: this.model
 		});
 		this.$el.html(renderedContent);
+		this.setViews();
 		this.renderStories();
 		return this;
 	},
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
-
+	
 	def root
+		if signed_in?
+		 render :root
+		else
+		 render :welcome
+		end
 	end
 end
